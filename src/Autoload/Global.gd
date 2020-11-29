70fb09ac87cd0a04f370853081229ccf9f837852
Relintai <relintai@gmail.com>
2020-11-29 15:17:11 +0100

    Rename root node again, but properly this time.

--- src/Autoload/Global.gd
+++ src/Autoload/Global.gd
@@ -192,7 +192,7 @@ func _ready() -> void:
 	Input.set_custom_mouse_cursor(DrawGD.cursor_image, Input.CURSOR_CROSS, Vector2(15, 15))
 
 	var root = get_tree().get_root()
-	control = find_node_by_name(root, "Control")
+	control = find_node_by_name(root, "DrawGDControl")
 	top_menu_container = find_node_by_name(control, "TopMenuContainer")
 	left_cursor = find_node_by_name(root, "LeftCursor")
 	right_cursor = find_node_by_name(root, "RightCursor")
