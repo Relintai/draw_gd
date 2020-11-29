175948b4ac7655e68db12a1fe1a7e5947a8935c9
Relintai <relintai@gmail.com>
2020-11-29 22:02:47 +0100

    Removed play button from the minimap.

--- addons/draw_gd/src/UI/CanvasPreviewContainer.gd
+++ addons/draw_gd/src/UI/CanvasPreviewContainer.gd
@@ -4,7 +4,6 @@ extends PanelContainer
 
 onready var canvas_preview = $HBoxContainer/PreviewViewportContainer/Viewport/CanvasPreview
 onready var camera : Camera2D = $HBoxContainer/PreviewViewportContainer/Viewport/CameraPreview
-onready var play_button : Button = $HBoxContainer/VBoxContainer/PlayButton
 
 var DrawGD : Node = null
 
@@ -13,14 +12,3 @@ func _on_PreviewZoomSlider_value_changed(value : float) -> void:
 	camera.save_values_to_project()
 	camera.update_transparent_checker_offset()
 
-
-func _on_PlayButton_toggled(button_pressed : bool) -> void:
-	if button_pressed:
-		if DrawGD.current_project.frames.size() <= 1:
-			play_button.pressed = false
-			return
-		canvas_preview.animation_timer.start()
-		DrawGD.change_button_texturerect(play_button.get_child(0), "pause.png")
-	else:
-		canvas_preview.animation_timer.stop()
-		DrawGD.change_button_texturerect(play_button.get_child(0), "play.png")
