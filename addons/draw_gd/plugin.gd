ae862905308cdd69fb0ea44435eaa3872cb20774
Relintai <relintai@gmail.com>
2020-11-29 15:47:14 +0100

    Implement main screen.

--- addons/draw_gd/plugin.gd
+++ addons/draw_gd/plugin.gd
@@ -2,6 +2,9 @@ tool
 extends EditorPlugin
 
 const _icon : Texture = preload("res://addons/draw_gd/icons/icon_copy.png")
+const _scene : PackedScene = preload("res://addons/draw_gd/src/Main.tscn")
+
+var main_node : Node = null
 
 func has_main_screen():
 	return true
@@ -16,10 +19,27 @@ func get_plugin_name():
 func _enter_tree():
 	#add_autoload_singleton("DrawGD", "res://addons/draw_gd/src/Autoload/DrawGD.gd")
 	
-	pass
+	main_node = _scene.instance()
+	
+	get_editor_interface().get_editor_viewport().add_child(main_node)
+	
+	make_visible(false)
+	
 
 func _exit_tree():
 	#remove_autoload_singleton("DrawGD")
 	
+	if main_node:
+		main_node.queue_free()
+
+
+func make_visible(visible):
+	if !main_node:
+		return
 	
-	pass
+	if visible:
+		main_node.show()
+	else:
+		main_node.hide()
+		
+		
