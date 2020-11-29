76b65f7433d98a452a4b40a6fd0e4b6fc94a721c
Relintai <relintai@gmail.com>
2020-11-29 15:34:09 +0100

    Started work on the plugin.

--- addons/draw_gd/plugin.gd
+++ addons/draw_gd/plugin.gd
@@ -1,10 +1,24 @@
 tool
 extends EditorPlugin
 
+func has_main_screen():
+	return true
+	
+func get_plugin_icon():
+	
+	return null
+	
+func get_plugin_name():
+	return "DrawGD"
+	
 
 func _enter_tree():
+	#add_autoload_singleton("DrawGD", "res://addons/draw_gd/src/Autoload/DrawGD.gd")
+	
 	pass
 
-
 func _exit_tree():
+	#remove_autoload_singleton("DrawGD")
+	
+	
 	pass
